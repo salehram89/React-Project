66a8d887f4a7352d42f3133a12306b86343ef1ec
salehram89 <saleh.ramadan@hotmail.co.uk>
2023-03-01 15:44:18 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: '447820dade170aab3486577b52a4d798',
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState('{}');
+
+  const search = evt => {
+    if (evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metrics&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+      });
+    }
+  }
+
+
+  const dateBuilder = (d) => {
+    let months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+    let days = [ 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' , 'Sunday'];
+
+    let day= days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
   return (
     <div className="app">
       <main>
@@ -11,7 +39,19 @@ function App() {
           <input 
           type='tex'
           className='search-bar'
-          placeholder='Search..'/>
+          placeholder='Search..'
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search} 
+          />
+        </div>
+        <div className='location-box'>
+          <div className='location'>New York City, Us</div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
+        <div className='weather-box'>
+          <div className='temp'>15°c</div>
+          <div className='weather'>Sunny</div>
         </div>
       </main>
     </div>
