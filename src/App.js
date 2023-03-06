75d20ccd197df8ddd05aea1184c028f7fdc88e55
salehram89 <saleh.ramadan@hotmail.co.uk>
2023-03-06 11:38:13 +0100

    new feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { type } from '@testing-library/user-event/dist/type';
 import React, { useState } from 'react';
 const api = {
-  key: '447820dade170aab3486577b52a4d798',
+  key: '447820dade170aab3486577b52a4d798', 
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
@@ -10,7 +10,7 @@ function App() {
 
   const search = evt => {
     if (evt.key === "Enter"){
-      fetch(`${api.base}weather?q=${query}&units=metrics&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=metrics&APPID=${api.key}&units=metric`)
       .then(res => res.json())
       .then(result => {
         setWeather(result);
@@ -25,7 +25,7 @@ function App() {
 
   const dateBuilder = (d) => {
     let months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
-    let days = [ 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' , 'Sunday'];
+    let days = ['Sunday', 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' ];
 
     let day= days[d.getDay()];
     let date = d.getDate();
@@ -60,7 +60,9 @@ function App() {
           <div className='date'>{dateBuilder(new Date())}</div>
         </div>
         <div className='weather-box'>
-          <div className='temp'>{Math.round(weather.main.temp)}°c</div>
+          <div className='temp'>
+            {Math.round(weather.main.temp)}°c
+            </div>
           <div className='weather'>{weather.weather[0].main}</div>
         </div>
         </div>
